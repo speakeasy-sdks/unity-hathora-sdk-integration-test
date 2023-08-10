115b7ff792ade4cb66d1533859557db6225d92cc
Manjit Bedi <dev@manjitbedi.com>
2023-08-10 12:13:21 -0700

    Chore: working on adding tests
    Note: code does not compile at this time; there is additional work required on the generated code.

--- Assets/Scripts/TestAPIController.cs
+++ Assets/Scripts/TestAPIController.cs
@@ -9,6 +9,12 @@ using Hathora.Cloud.Sdk.Client;
 using Hathora.Cloud.Sdk.Model;
 using Hathora.Core.Scripts.Runtime.Client;
 
+using SpeakeasyHathora;
+using SpeakeasyHathora.Models.RoomV2;
+using SpeakeasyHathora.Models.Shared;
+using CreateRoomRequest = SpeakeasyHathora.Models.RoomV2.CreateRoomRequest;
+
+
 /// <summary>
 /// Test controller to use with Speakeasy generated code
 /// </summary>
@@ -28,9 +34,31 @@ public class TestAPIController : MonoBehaviour
 
     #region UI
 
-    public void CreateRoom()
+    public async Task CreateRoomAsync()
     {
         Debug.Log("Create room using Speakeasy");
+
+        var sdk = new SpeakeasyHathoraSDK();
+
+        var auth = "";
+        var appId = "app-af469a92-5b45-4565-b3c4-b79878de67d2";
+        var roomId = "2swovpy1fnunu";
+
+        // TODO: update code
+        //var res = await sdk.RoomV2.CreateRoomAsync(
+        //    new CreateRoomSecurity()
+        //    {
+        //        Auth0 = auth,
+        //    },
+        //    new CreateRoomRequest()
+        //    {
+        //        CreateRoomRequestValue = "",
+        //        AppId = appId,
+        //        RoomId = roomId,
+        //    });
+
+        // examine the room info
+
     }
 
     public void GetConnectionInfoSpeakeasyAsync()
--- README.md
+++
@@ -1,3 +0,0 @@
-# unity-hathora-sdk-integration-test
-
-Work area for using the Speakeasy generared Unity openapi code with the demo scenes from Hathora.
